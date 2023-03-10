e3ff7ccf7f58f977c9b55c6af63ce483dcaffa86
alexangria1992 <alex040892@gmail.com>
2023-03-10 22:52:50 +1300

    Banner Section

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import "./Home.scss";
+
+import Banner from "./Banner/Banner";
+
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <Banner />
+    </div>
+  );
 };
 
 export default Home;
