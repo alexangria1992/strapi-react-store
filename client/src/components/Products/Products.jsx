11f26b9fc5e75b6887cb1fb139ad87739b424f9b
alexangria1992 <alex040892@gmail.com>
2023-03-15 15:36:43 +1300

    Category Page API integration

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -8,7 +8,7 @@ const Products = ({ products, innerPage, headingText }) => {
       {!innerPage && <div className="sec-heading">{headingText}</div>}
 
       <div className="products">
-        {products.data.map((item) => (
+        {products?.data?.map((item) => (
           <Product key={item.id} id={item.id} data={item.attributes} />
         ))}
       </div>
